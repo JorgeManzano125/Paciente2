fafd3b81c0e4072065f2f85e03bf747af7b11965
JorgeManzano125 <capi-jorge_bus@hotmail.com>
2015-04-28 19:33:04 +0200

    2

--- Paciente2/src/Paciente/Paciente.java
+++ Paciente2/src/Paciente/Paciente.java
@@ -1,5 +1,10 @@
 package Paciente;
 
 public class Paciente {
-
+	private String nombre;
+	private String estado;
+	public Paciente(String nombre, String estado) {
+		this.nombre = nombre;
+		this.estado = estado;
+	}
 }
