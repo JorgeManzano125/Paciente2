95926c9a0a4961c5f1870356f2c0856c94d929cc
SERVIDOR_JUAN <SERVIDOR_JUAN@SERVIDOR>
2015-05-07 19:54:39 +0200

    1

--- Paciente2/src/Paciente/Paciente.java
+++ Paciente2/src/Paciente/Paciente.java
@@ -3,8 +3,14 @@ package Paciente;
 public class Paciente {
 	private String nombre;
 	private String estado;
+	
 	public Paciente(String nombre, String estado) {
 		this.nombre = nombre;
 		this.estado = estado;
+	}
+	
+	@Override
+	public int compareTo(Object o){
+		return 1;
 	}
 }
